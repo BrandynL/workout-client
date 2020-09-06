e5bbfa45a51cde1ef0a8a05b574efe114e224c5f
BrandynL <brandyn.lordi@gmail.com>
2020-09-05 23:55:11 -0400

    added initial api fetch and verbiage changes

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Container, Box } from '@material-ui/core';
 import LoginForm from '../components/forms/LoginForm';
+import { Link } from 'react-router-dom';
 
 export default function LoginScreen() {
 	return (
 		<Container>
-			<Box maxWidth={600} m='auto'>
+			<Box
+				minHeight='100vh'
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				alignItems='center'
+				maxWidth={600}
+				m='auto'
+			>
+				<h1>Login to your account</h1>
 				<LoginForm />
+				<Box my={5} textAlign='center'>
+					<Link to='/register'>Create an account</Link>
+				</Box>
 			</Box>
 		</Container>
 	);
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Container, Box } from '@material-ui/core';
+import { Container, Box, Button } from '@material-ui/core';
 import RegisterForm from '../components/forms/RegisterForm';
+import { Link } from 'react-router-dom';
 
 export default function RegisterScreen() {
 	return (
 		<Container>
-			<Box maxWidth={600} m='auto'>
+			<Box
+				minHeight='100vh'
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				alignItems='center'
+				maxWidth={600}
+				m='auto'
+			>
+				<h1>Create an account</h1>
 				<RegisterForm />
+				<Box my={5} textAlign='center'>
+					<Link to='/login'>Already have an account?</Link>
+				</Box>
 			</Box>
 		</Container>
 	);
