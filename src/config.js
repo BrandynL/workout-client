aaaeea865dc94660e6aecd6f70024f7c3c60235a
BrandynL <brandyn.lordi@gmail.com>
2020-09-05 23:31:55 -0400

    added prod and dev config for api access

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const dev = {
+	api: {
+		base: 'https://127.0.0.1:8000',
+		endpoints: {
+			user_auth: '/api/v1/auth',
+			user_register: '/api/v1/register',
+		},
+	},
+};
+
+const prod = {};
+
+export const api = process.env.NODE_ENV === 'development' ? dev.api : prod.api;
