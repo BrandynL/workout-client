26fa0e57ff591bf97f1ef430b94ff73208485aa0
BrandynL <brandyn.lordi@gmail.com>
2020-09-09 17:30:45 -0400

    added nested objects to add flexibility

--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
-const dev = {
-	api: {
-		v1: {
-			base: 'https://127.0.0.1:8000',
-			endpoints: {
-				user_auth: '/api/v1/auth',
-				user_register: '/api/v1/register',
+const API = {
+	dev: {
+		api: {
+			v1: {
+				base: 'https://127.0.0.1:8000',
+				endpoints: {
+					user_auth: '/api/v1/auth',
+					user_register: '/api/v1/register',
+				},
 			},
 		},
 	},
+	prod: {},
 };
 
-const prod = {};
-
-export const api = process.env.NODE_ENV === 'development' ? dev.api : prod.api;
+export const api =
+	process.env.NODE_ENV === 'development' ? API.dev.api : API.prod.api;
