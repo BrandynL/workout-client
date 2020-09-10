5d631943bde198e5cf617b73d3c5beb99da19f7a
BrandynL <brandyn.lordi@gmail.com>
2020-09-09 20:20:43 -0400

    added user state token default value

--- src/Context/UserContext/UserContext.js
+++ src/Context/UserContext/UserContext.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import AuthTokenHelperService from '../../helperServices/AuthTokenHelperService';
 
 export const UserContext = React.createContext({ user: {} });
 
 export const UserContextProvider = ({ children }) => {
-	const [user, setuser] = useState({});
+	const tokenHelper = new AuthTokenHelperService();
+	const [user, setuser] = useState(tokenHelper.getDecodedToken());
+
 	const context = { user, setuser };
 	return (
 		<UserContext.Provider value={context}>{children}</UserContext.Provider>
